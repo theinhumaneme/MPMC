1ffc1bec490bd5ec9ad0bc844bb7eab4bf4a4181
theinhumaneme <theinhumaneme@gmail.com>
2022-02-23 15:01:31 +0530

    DFT with Padding DSP LAB

---
+++ DSP Lab/DFT && IDFT/DFT with Padding.m
@@ -0,0 +1,34 @@
+clc;
+clear all;
+close all;
+N = input('Enter total number of samples');
+n = 0:1:N-1;
+x = input('Enter the corresponding values of x');
+if length(x)<N
+    x([length(x)+1:N]) = zeros(1,N-length(x)); % Adding the extra zeros 
+end
+k = 0:1:N-1;
+wn = exp((-j*2*pi)./N);
+kn = n'*k;
+wk = wn.^kn;
+Xk = x*wk;
+mag = abs(Xk);
+phase = angle(Xk)*(180/pi)
+subplot(3,1,1);
+stem(x,n);
+xlabel('Frequency')
+ylabel('Amplitude')
+legend('19R11A04N1')
+title('DFT Plot')
+subplot(3,1,2);
+stem(k,mag);
+xlabel('Frequency')
+ylabel('Magnitude')
+legend('19R11A04N1')
+title('Magnitude Plot of DFT')
+subplot(3,1,3);
+stem(k,phase)
+xlabel('Frequency')
+ylabel('Phase')
+legend('19R11A04N1')
+title('Phase Plot of DFT')
